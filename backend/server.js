06f83a90d1711474cb2d2c9e0704ffbcfca14a94
abhishek <abhismalviya@outlook.com>
2021-07-24 11:57:01 +0530

    jwt in static

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const busboy = require("connect-busboy");
 const cors = require("cors");
+const AuthValidationMiddleware = require('./app/http/middleware/auth/auth.validation.middleware');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use([express.static("public")]);
+
+let x = 0;
+
+app.use([
+	(req, res, next) => {
+		console.log("Hello world", x++);
+		next();
+	},
+	AuthValidationMiddleware.validJWTNeeded,
+	express.static("public"),
+]);
 
 app.use(cors());
 
@@ -16,6 +27,7 @@ app.use(
 		highWaterMark: 2 * 1024 * 1024,
 	})
 );
+
 
 require("./routes/routes.api.user").routesConfig(app);
 require("./routes/routes.api.auth").routesConfig(app);
