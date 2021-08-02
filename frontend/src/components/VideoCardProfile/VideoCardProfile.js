aeca915fa44710b77185bcf6a5f3a58a8e5ec91c
abhishek <abhismalviya@outlook.com>
2021-08-03 01:22:23 +0530

    profile

--- frontend/src/components/VideoCardProfile/VideoCardProfile.js
+++ frontend/src/components/VideoCardProfile/VideoCardProfile.js
@@ -5,11 +5,9 @@ function VideoCardProfile(props) {
 	return (
 		<div className={classes.container}>
 			<img className={classes["video-img"]} src="images/default.png" />
-
 			<div className={classes.details}>
-				<div> Tiel </div>
-				<div> Status: Shared </div>
-        <input type="radio" />
+				<div> Title </div>
+				<div> Shared</div>
 			</div>
 		</div>
 	);
