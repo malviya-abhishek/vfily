e942b5fdaef2862995b760b11587f59c6f9cc7ee
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2021-10-04 02:18:01 +0530

    cookie

--- backend/cookie.setting.js
+++ backend/cookie.setting.js
@@ -1,6 +1,10 @@
+var now = new Date();
+now.setTime(now.getTime() + 1 * 3600 * 1000*1000);
+
 exports.cookieSetting = {
 	sameSite: "none",
 	path: "/",
 	secure: true,
 	httpOnly: true,
+	expires: now.toUTCString()
 };
