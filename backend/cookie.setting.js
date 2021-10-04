0695d971132cdaac21b9479a1cc51ef8a14148b0
Abhishek Malviya <71405686+malviya-abhishek@users.noreply.github.com>
2021-10-04 12:44:32 +0530

    cookie factory function setter

--- backend/cookie.setting.js
+++ backend/cookie.setting.js
@@ -1,10 +1,9 @@
-var now = new Date();
-now.setTime(now.getTime() + 1 * 3600 * 1000*1000);
-
-exports.cookieSetting = {
-	sameSite: "none",
-	path: "/",
-	secure: true,
-	httpOnly: true,
-	expires: now.toUTCString()
+exports.cookieSetter = ()=>{ 
+	return {
+		sameSite: "none",
+		path: "/",
+		secure: true,
+		httpOnly: true,
+		expires: new Date(new Date().getTime() + 1000*60*60*24),
+	}
 };
